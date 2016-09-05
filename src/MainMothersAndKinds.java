e473f6b3854c856e55e99aadb0b8c1da4f338947
jola <jola.j.wk@gmail.com>
2016-09-05 21:36:55 +0200

    SearchService

--- src/MainMothersAndKinds.java
+++ src/MainMothersAndKinds.java
@@ -1,12 +1,23 @@
 import java.util.List;
 
 public class MainMothersAndKinds {
+
 	public static void main(String[] args) {
 		List<Mother> mothers = MotherFactory.getMothers();
 		mothers.forEach(System.out::println);
 		List<Kid> kids = KidFactory.getKids(mothers);
 		kids.forEach(System.out::println);
 		System.out.println("###");
+		SearchService ss = new SearchService();
+		System.out.println(ss.heaviestKid(kids));
+		System.out.println("###");
+		System.out.println(ss.listOfKidsYearly("1999-11-17", kids).toString().replace(" ","\n"));
+		System.out.println("###");
+		System.out.println(ss.numberOfKidsYearly("1999", kids));
+		System.out.println("###");
 		System.out.println(kids.get(0).getMother());
+		System.out.println(ss.nrOfKidsPerMother(kids).toString().replace(" M","\n"));
+		System.out.println("###");
+		System.out.println(ss.mothersWithMostKids(mothers));
 	}
 }
--- src/SearchService.java
+++ src/SearchService.java
@@ -1,4 +1,40 @@
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.logging.Filter;
+import java.util.stream.Collectors;
 
 public class SearchService {
-
+//	1.matka ktora ma najwiecej dzieci
+//	2. naciezsze dziecko
+//	3. zestawienie rok urodzenia jaka liczba dzieci
+	
+	public Mother mothersWithMostKids(List<Mother> mothers){
+		//problem kiedy maja taka sama ilosc dzieci !!!
+		return mothers.stream().collect
+				(Collectors.maxBy((o1, o2) -> o1.getKids().size() - o2.getKids().size())).get();
+		
+	}
+	
+	public Map<Mother, Long> nrOfKidsPerMother(List<Kid> kids){
+		return kids.stream().collect(Collectors.groupingBy
+				(e -> e.getMother(), Collectors.counting()));
+		
+	}
+	
+	public Kid  heaviestKid(List<Kid> kids){
+		return kids.stream().collect(Collectors.maxBy(
+				(o1, o2) -> o1.getWeight() - o2.getWeight())).get();
+		}
+	
+	public  long numberOfKidsYearly(String year, List<Kid>kids){
+		return kids.stream().filter(p -> p.getBirthDate().contains(year)).count(); 
+	}
+	
+	public  Map<String, Long> listOfKidsYearly(String year, List<Kid>kids){
+		
+		return kids.stream().collect(Collectors.groupingBy
+				(e -> e.getBirthDate(), Collectors.counting()));
+	}
+	
 }
